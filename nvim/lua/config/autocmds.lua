02660a6118ba51f2366df0681d18a968a183bae9
Wendell <wendellwangxinyu@icloud.com>
2024-03-13 01:51:14 +0800

    Remove unused config

--- nvim/lua/config/autocmds.lua
+++
@@ -1,8 +0,0 @@
-local autocmd = vim.api.nvim_create_autocmd
-
-autocmd("QuitPre", {
-  callback = function()
-    -- Reset the cursor style on exit
-    os.execute('printf "\\e[5 q" > $(tty)')
-  end
-})
