123330b95c9b2ce2467a3a4041bce078a2a4e26e
Wendell <wendellwangxinyu@icloud.com>
2024-03-13 02:44:00 +0800

    Add formater and linter support for Swift

---
+++ nvim/lua/plugins/format.lua
@@ -0,0 +1,32 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        swift = { "swift_format" },
+        lua = { "stylua" },
+        -- Use the "*" filetype to run formatters on all filetypes.
+        ["*"] = { "codespell" },
+        -- Use the "_" filetype to run formatters on filetypes that don't
+        -- have other formatters configured.
+        ["_"] = { "trim_whitespace" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true
+      },
+      log_level = vim.log.levels.ERROR,
+    })
+
+    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500,
+      })
+    end, { desc = "Format file or range (in visual mode)" })
+  end,
+}
