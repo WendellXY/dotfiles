1bcb3b6d0682efe458187fabe4824a531428cec0
Wendell <41480456+WendellXY@users.noreply.github.com>
2024-03-13 11:44:05 +0800

    Dynamically pick a scheme based on the system's color scheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,20 @@
+local configPath = vim.fn.stdpath('config') -- Get the path to the Neovim configuration directory
+local swiftScriptPath = configPath .. "/utils/DetectColorScheme.swift"
+
 return {
   "catppuccin/nvim",
   lazy = false,
   name = "catppuccin",
   priority = 1000,
   config = function()
+    local result = vim.fn.system("swift " .. swiftScriptPath):gsub("%s+", "")
+    local flavour = "mocha" -- Default to mocha
+    if result == "light" then
+      flavour = "latte"
+    end
     require("catppuccin").setup({
-      transparent_background = true
+      transparent_background = true,
+      flavour = flavour,
     })
     vim.cmd.colorscheme "catppuccin"
   end
