1bd7e655f38d29fd8ea20039c38eea0b5f159e2e
Wendell <41480456+WendellXY@users.noreply.github.com>
2024-03-16 16:15:11 +0800

    Share vim clipboard to system

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,4 +3,5 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
-vim.g.mapleader=" "
+vim.g.mapleader = " "
+vim.opt.clipboard = "unnamedplus"
