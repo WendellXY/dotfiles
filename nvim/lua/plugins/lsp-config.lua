e90df972730a01c64f4daa06654c3bcf3a2c5a8c
Wendell <wendellwangxinyu@icloud.com>
2024-03-19 19:01:15 +0800

    Drop `root_dir` check for swift

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -29,12 +29,11 @@ return {
 
       lspconfig["sourcekit"].setup({
         capabilities = capabilities,
-        root_dir = lspconfig.util.root_pattern('Package.swift'),
       })
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
     end
   }
 }
