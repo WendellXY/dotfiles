add10f0179f801029c1877c330411a1b971cdebb
Wendell <wendellwangxinyu@icloud.com>
2024-03-25 18:30:28 +0800

    Disable dynamic color scheme of wezterm

--- wezterm/appearance.lua
+++ wezterm/appearance.lua
@@ -1,19 +1,19 @@
-local wezterm = require "wezterm"
-
-local function get_appearance()
-  if wezterm.gui then
-    return wezterm.gui.get_appearance()
-  end
-  return "Dark"
-end
-
-local function scheme_for_appearance(appearance)
-  if appearance:find "Dark" then
-    return "Ocean (dark) (terminal.sexy)"
-  else
-    return "Ocean (light) (terminal.sexy)"
-  end
-end
+-- local wezterm = require "wezterm"
+--
+-- local function get_appearance()
+--   if wezterm.gui then
+--     return wezterm.gui.get_appearance()
+--   end
+--   return "Dark"
+-- end
+--
+-- local function scheme_for_appearance(appearance)
+--   if appearance:find "Dark" then
+--     return "Ocean (dark) (terminal.sexy)"
+--   else
+--     return "Ocean (light) (terminal.sexy)"
+--   end
+-- end
 
 local module = {}
 
@@ -21,8 +21,9 @@ function module.set(config)
   config.default_cursor_style = "BlinkingUnderline"
   config.hide_tab_bar_if_only_one_tab = true
   config.window_decorations = "NONE | RESIZE"
+  config.color_scheme = 'Tomorrow Night Bright'
 
-  config.color_scheme = scheme_for_appearance(get_appearance())
+  -- config.color_scheme = scheme_for_appearance(get_appearance())
 end
 
 return module
