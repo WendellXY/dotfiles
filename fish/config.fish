f0f66bd77bce08b648a765322f27ac3808819a37
Wendell <root@tzeentch.io>
2024-08-02 18:01:35 +0800

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -2,13 +2,16 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# Zoxide Configs
+# Initialize various tools and plugins
+# zoxide: Fast directory navigation tool
 zoxide init fish --cmd cd | source
 
-# Fzf Configs
+# fzf: Fuzzy finder configuration
 fzf --fish | source
+set -Ux FZF_DEFAULT_COMMAND "rg --files"
+set -Ux FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 
-# Starship Configs
+# Starship: Cross-shell prompt with extensive customization
 function starship_transient_prompt_func
     starship module character
 end
