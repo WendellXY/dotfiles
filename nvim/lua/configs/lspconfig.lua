a0bfdba71c596e819fb835e29d94e156ee90d6dd
Wendell <root@tzeentch.io>
2024-11-13 19:52:03 +0800

    add python lsp

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,13 +1,10 @@
--- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
--- EXAMPLE
-local servers = { "html", "cssls", "nushell" }
+local servers = { "html", "cssls", "nushell", "pyright" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = nvlsp.on_attach,
