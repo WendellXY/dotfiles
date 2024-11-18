3b1675718a9f57e6d908131ddb714253a166565e
Wendell <root@tzeentch.io>
2024-11-18 14:08:42 +0800

    Update chadrc.lua

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -1,8 +1,7 @@
 -- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
 -- Please read that file to know all available options :(
-
----@type ChadrcConfig
+--- @type ChadrcConfig
 local M = {}
 
 M.base46 = {
