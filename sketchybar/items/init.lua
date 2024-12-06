06991646ec9c4264cb83d3a068d8aa36d2407712
Wendell <root@tzeentch.io>
2024-12-06 08:46:47 +0800

    Remove unused configuration

--- sketchybar/items/init.lua
+++
@@ -1,8 +0,0 @@
-require "items.apple"
-require "items.menus"
-require "items.spaces"
-require "items.front_app"
-require "items.calendar"
-require "items.widgets"
-require "items.badges"
-require "items.media"
