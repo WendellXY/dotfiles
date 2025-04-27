0b8c6a533343d5ae5528e687c5c12f8f8f2513ee
Wendell <root@tzeentch.io>
2025-04-27 18:49:17 +0800

    Revert nui.nvim to 8d5b0b to fix notice.nvim's compatibility issue

--- nvim/lua/plugins/basics.lua
+++ nvim/lua/plugins/basics.lua
@@ -152,7 +152,10 @@ return {
 			},
 		},
 		dependencies = {
-			"MunifTanjim/nui.nvim",
+			{
+				"MunifTanjim/nui.nvim",
+				commit = "8d3bce9764e627b62b07424e0df77f680d47ffdb",
+			},
 			"rcarriga/nvim-notify",
 		},
 	},
