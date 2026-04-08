3d14c8261df5e2051670066fbe7c11971ea99066
Wendell <root@tzeentch.io>
2026-04-08 14:25:57 +0800

    Revert "perf(zsh): speed up fastfetch on shell startup"
    This reverts commit bddaf8460d251c7d8f6754239d7f36faf6f3d9fe.

--- zsh/initial.zsh
+++ zsh/initial.zsh
@@ -1,11 +1,3 @@
-if [[ -o interactive ]] \
-  && [[ -t 1 ]] \
-  && [[ -z "${FASTFETCH_DISABLE_AUTO:-}" ]] \
-  && [[ -z "${TMUX:-}" ]] \
-  && [[ -z "${FASTFETCH_SHOWN:-}" ]] \
-  && (( ${SHLVL:-0} == 1 )) \
-  && (( $+commands[fastfetch] )); then
-  # fastfetch is informational only, so keep nested shells responsive.
-  export FASTFETCH_SHOWN=1
-  fastfetch --detect-version false --structure Title:OS:Host:Kernel:CPU:GPU:Memory:Disk:Battery
+if [[ -o interactive ]] && type fastfetch &>/dev/null; then
+  fastfetch
 fi
